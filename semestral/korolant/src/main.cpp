66999bdc9e4cffd7396378b8af6744fc86afe3fe
Anton <korolant@fit.cvut.cz>
2021-06-03 22:57:43 +0200

    +README.md edit doxyfile and makefile

--- semestral/korolant/src/main.cpp
+++ semestral/korolant/src/main.cpp
@@ -1,7 +1,6 @@
 #include "Application/CApplication.h"
 using namespace std;
 /**
- * @mainpage Calendar
  * Application "Calendar" with the most common and useful commands, which are necessary to  plan your life our other
  * activities with comfort.
  */
