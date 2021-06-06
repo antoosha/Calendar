de625a1b5039ee359d13f78cfdec9b1b79c0bdf5
Anton <korolant@fit.cvut.cz>
2021-06-07 00:48:53 +0200

    date

--- semestral/korolant/src/Calendar/CDate.cpp
+++ semestral/korolant/src/Calendar/CDate.cpp
@@ -27,12 +27,9 @@ int CDate::returnYear() const {
 std::string CDate::dateToString(const CDate & cDate) const {
     char s1[13];
     s1[12] = '\0';
-    int yearIn = cDate.year;
-    int monthIn = cDate.month;
-    int dayIn = cDate.day;
-    int hourIn = cDate.hour;
-    int minuteIn = cDate.minute;
-    std::sprintf(s1, "%04d%02d%02d%02d%02d",yearIn, monthIn, dayIn, hourIn, minuteIn);
+    if(cDate.year && cDate.month &&  cDate.day && cDate.hour && cDate.minute)
+        std::sprintf(s1, "%04d%02d%02d%02d%02d",cDate.year, cDate.month, cDate.day, cDate.hour, cDate.minute);
+    else throw "problem with date";
     string dateInString(s1);
     return dateInString;
 }
